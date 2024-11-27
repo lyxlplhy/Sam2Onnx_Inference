25a2775f5c84cf14ef0af2a083b64587ebf23463
lyxlplhy <1655080378@qq.com>
2024-11-27 11:41:56 +0800

    新增yolo目标检测作为sam2提示信息

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-#include <filesystem>
 #include "SAM2.h"
+#include<string>
+#include <filesystem>
+#include<regex>
+#include"yolov8Predictor.h"
 #include <onnxruntime_cxx_api.h>
 
 void sam2() {
     std::vector<std::string> onnx_paths{
-        "D:/sam2_c++/Project1/model/model_sam2/tiny_encoder.onnx",
-        "D:/sam2_c++/Project1/model/model_sam2/tiny_decoder.onnx",
+        "D:/sam2/segment-anything-2-main/tools/conver_tiny_encoder.onnx",
+        "D:/sam2/segment-anything-2-main/tools/conver_tiny_encoder.onnx",
     };
     auto sam2 = std::make_unique<SAM2>();
     auto r = sam2->initialize(onnx_paths,false);
@@ -16,7 +19,7 @@ void sam2() {
         return;
     }
 
-    int type = 0;
+    int type = 1;
     cv::Rect prompt_box = { 1087,1200,1000,1000 };//xywh
     cv::Point prompt_point = { 1373, 1682 };
     sam2->setparms(type, prompt_box, prompt_point); // 在原始图像上的box,point
@@ -58,25 +61,80 @@ void sam2() {
     capture.release();
 }
 
+void yolo()
+{
+    const std::vector<std::string> classNames = {"conver"};
+    YOLOPredictor predictor{ nullptr };
+    bool isGPU = false;
+    std::string modelPath = "D:/sam2/ultralytics-main/ultralytics-main/runs/detect/train4/weights/best.onnx";//yolo
+    std::vector<std::string> onnx_paths{
+        "D:/sam2/segment-anything-2-main/tools/conver_model/conver_tiny_encoder.onnx",
+        "D:/sam2/segment-anything-2-main/tools/conver_model/conver_tiny_decoder.onnx",
+    };//sam2
+    std::string Filename = "E:/LYX_date/yanwo_cover/1_yanwo_cover_data/Image_20240925140308619.jpg";
+    float confThreshold = 0.4f;
+    float iouThreshold = 0.4f;
+    float maskThreshold=0.5f;
+    try
+    {
+        predictor = YOLOPredictor(modelPath, isGPU,
+            confThreshold,
+            iouThreshold,
+            maskThreshold);
+        std::cout << "Model was initialized." << std::endl;
+        assert(classNames.size() == predictor.classNums);
+        std::regex pattern(".+\\.(jpg|jpeg|png|gif)$");
+        std::cout << "Start predicting..." << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    auto sam2 = std::make_unique<SAM2>();
+    auto r = sam2->initialize(onnx_paths, false);
+    if (r.index() != 0) {
+        std::string error = std::get<std::string>(r);
+        std::cout << ("错误：{}", error);
+        return;
+    }
+    int type = 1;//0用框，1用点
+    cv::Mat image = cv::imread(Filename);
+    std::vector<Yolov8Result> result = predictor.predict(image);
+    //utils::visualizeDetection(image, result, classNames);
+    for (int idx = 0;idx < result.size();++idx)
+    {
+        cv::Point prompt_point = { result[idx].box.x+result[idx].box.width/2,result[idx].box.y + result[idx].box.height / 2 };
+        sam2->setparms(type, result[idx].box, prompt_point); // 在原始图像上的box,point
+        cv::Mat sam2_frame = cv::imread(Filename);
+        auto result = sam2->inference(sam2_frame);
+        if (result.index() == 0) {
+            std::string text = "frame = " + std::to_string(idx) ;
+            cv::putText(sam2_frame, text, cv::Point{ 30,40 }, 1, 2, cv::Scalar(0, 0, 255), 2);
+            cv::imshow("window_name", sam2_frame);
+            int key = cv::waitKey(0);
+            if (key == 'q' || key == 27) break;
+        }
+        else {
+            std::string error = std::get<std::string>(result);
+            std::cout << ("错误：{}", error);
+            break;
+        }
+
+    }
+
+}
+
 bool isGpuAvailable() {
     try {
-        // 创建一个 ONNX Runtime 环境
         Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "ONNXRuntime");
-
-        // 使用 GPU（CUDA）提供商配置选项
         Ort::SessionOptions session_options;
         session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
-
-        // 尝试将执行提供商设置为 CUDA
 #if defined(ORT_CUDA)
         session_options.AppendExecutionProvider_CUDA(0); // 使用第0号 GPU
 #else
         std::cerr << "CUDA is not enabled in this build of ONNX Runtime." << std::endl;
         return false;
 #endif
-
-        // 创建一个空的 session，用于测试 CUDA 提供商是否正常工作
-        // 假设模型文件 "dummy_model.onnx" 存在并且是一个小型模型，用于测试 GPU 可用性
         const ORTCHAR_T* modelPath = L"D:/sam2/SAM2Export-main/image_encoder.onnx";
         Ort::Session test_session(env, modelPath, session_options);
 
@@ -91,7 +149,7 @@ bool isGpuAvailable() {
 
 int main(int argc, char const* argv[]) {
 
-    sam2();
+    yolo();
     return 0;
 }
 
